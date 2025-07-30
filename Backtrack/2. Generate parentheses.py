7735466deb031c32c2d527d0ae2ee51371fc73ee
Quant1wen <w2021201493@163.com>
2025-07-30 17:08:41 +0800

    Update 2. Generate parentheses.py

--- Backtrack/2. Generate parentheses.py
+++ Backtrack/2. Generate parentheses.py
@@ -5,21 +5,29 @@
 #Output: ["((()))","(()())","(())()","()(())","()()()"]
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res == []
+    def generateParenthesis(self, n: int) -> list[str]:
+        res = []
         def dfs(left,right,comb):
-            if len(comb) == 2*n:
+
+            # 边界条件：如果括号已经满足
+            if left == n and right == n:
                 res.append(comb)
+                return
 
-            #
-            if left < n:
-                dfs(left+1,right,comb+'(')
-            if left > right:
-                dfs(left,right+1,comb+')')
+            # 与下一层递归的嵌套关系：从头开始，本来是(0,0)，如何同时伸展到(1,0)/(0,1)?需要先到(1,0)，
+            #再到(0,1)：(0,0)满足left<=n到(1,0)，然后right<left，到(0,1)，每一层都通过折线的方式到下一层的两个分支
+            if left <= n:
+                dfs(left+1, right,comb+'(')
+            if right < left:
+                dfs(left, right+1,comb+')')
         dfs(0,0,'')
-
-
+        return res
 #      ((
 # (    
 #      ()
-# Fist layer - second layer 在回溯的同时还需要考虑每一层的所有情况 即if语句中的操作是每一层深度搜索的操作，而if语句的并列代表了每一层所有情况的包含
+# Fist layer - second layer 在回溯的同时还需要考虑每一层的所有情况 
+# 即if语句中的操作是每一层深度搜索的操作，而if语句的并列代表了每一层所有情况的包含
+
+Sol=Solution()
+n=3
+print(Sol.generateParenthesis(n))
